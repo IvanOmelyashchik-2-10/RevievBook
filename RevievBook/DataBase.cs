4e0746b78235aa05c7cbefd6f3e37f6b45a68647
Иван Омелящик <cpt.legion.337@gmail.com>
2022-12-09 17:06:59 +0300

    Подключение баззы данных MySql

---
+++ RevievBook/DataBase.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevievBook
+{
+    internal class DataBase
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost; port=3306;username=root;password=root;database=revievbook");
+
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+
+        }
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+
+        }
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
